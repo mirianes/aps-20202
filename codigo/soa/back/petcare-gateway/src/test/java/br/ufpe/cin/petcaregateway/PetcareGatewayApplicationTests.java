0695e530f5049cfc5742c167eb3fd358e64fcc6d
Miriane Nascimento <>
2021-08-14 17:24:24 -0300

    gateway com dockerfile

---
+++ codigo/soa/back/petcare-gateway/src/test/java/br/ufpe/cin/petcaregateway/PetcareGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.petcaregateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
